28c5ad265ebdfd04079caafdfd894ef487c04638
debjitttdasss <rossdebb452@gmail.com>
2026-03-18 00:32:16 +0530

    logicall
    work on the
    isloggedin and than ispaid
    
    && and || and other on like things only

---
+++ js fodundation/hello.js/part1/logical.js
@@ -0,0 +1,14 @@
+// && -> and
+// || -> or
+// ! -> reverse the value of a boolean
+
+let isLoggedIn = true;
+let ispaid = true;
+
+console.log(isLoggedIn && ispaid);
+// console.log(isLoggedIn && ispaid);
+
+
+let isemailuser = true;
+let isgoogleuser = false;
+console.log(isemailuser || isgoogleuser);
