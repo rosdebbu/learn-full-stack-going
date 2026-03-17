cdc6890aea77bd098c95e6016c23d33855362d48
debjitttdasss <rossdebb452@gmail.com>
2026-03-17 23:29:27 +0530

    datatype
    like noraml datatype asa js a
    
    var ra valo na
    replace a amda tor let use kori good to use

---
+++ js fodundation/hello.js/part1/datatype.js
@@ -0,0 +1,33 @@
+console.log("hey, its debjit!");
+
+/* This is a comment
+
+string
+number
+boolean
+Bigint
+
+undefined
+null
+
+object
+
+symbol
+
+var onok = "propermalitic";
+modern world a let use hoi
+*/
+// var score = 103.5;
+
+let score = 103.5;
+let  name = "debjit";
+let isLoggedIn = false;
+
+//object
+let teatypes = ["black tea", "green tea", "lemon tea" ,"alu tea"];
+let user = {firstName: "debjit", lastName:"das", age: 24, isLoggedIn: false};
+
+
+let getscore = score;
+console.log(getscore);
+
