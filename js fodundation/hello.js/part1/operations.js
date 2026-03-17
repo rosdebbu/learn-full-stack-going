cd18a82e059b63eaa7f7457d47a62532437ea1a2
debjitttdasss <rossdebb452@gmail.com>
2026-03-18 00:25:33 +0530

    operations on the js
    operations on the js
    like the addition
    sub
    multi
    divi
    module
    reminder
    expo
    myscore   -- myscore++
    credits    -- cresits-- this goes on

---
+++ js fodundation/hello.js/part1/operations.js
@@ -0,0 +1,44 @@
+let addition = 5 + 10;
+
+let score = 102;
+let bonus = 50;
+
+let totalScore = score + bonus;
+
+let addition1 = 5 + 10;
+let subtraction = 10 - 5;
+let mult= 3 * 3;
+let divi = 10 / 2;
+let modulus = 10 % 3;
+
+
+let remainder = 10 % 3;
+let expo = 2 ** 3; // This is the exponentiation operator, equivalent to Math.pow(2, 3)
+
+let myscore = 100;
+myscore++
+
+let credits = 100;
+credits--;
+
+// comparion operators 
+
+let num1 = 10;
+let num2 = 3;
+let num3 = 10;
+
+console.log(num1 > num2); // true
+console.log(num1 < num2); // false
+console.log(num1 >= num3); // true
+console.log(num1 != num3); // false
+console.log(num1 != num2); // true
+
+console.log(addition);
+console.log(totalScore);
+console.log(addition1);
+console.log(subtraction);
+console.log(mult);
+console.log(divi);
+console.log(modulus);
+console.log(remainder);
+console.log(expo);
