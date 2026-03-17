1f503ebe3aa081caedd6cf829bd833199117259a
debjitttdasss <rossdebb452@gmail.com>
2026-03-17 23:27:59 +0530

    for print
    eda ta normal print like the console
    2 process stdout eda o same words ba value ra print kora

---
+++ js fodundation/hello.js/part1/priting.js
@@ -0,0 +1,11 @@
+console.log("hey, its debjit!");
+console.log("hey, its debjit!");
+
+process.stdout.write("debjit hero section!");
+process.stdout.write("deb yo!");
+
+
+console.table({ name: "debjit", age: 24, city: "kolkata" });
+console.warn("this is a warning message");
+
+
