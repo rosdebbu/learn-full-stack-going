137830e716ff64daa2a1f03873f5ee0ef1809fd7
debjitttdasss <rossdebb452@gmail.com>
2026-03-17 23:26:58 +0530

    changes
    eda ta mainlky tor
    1 let hoila amda value ba word ar arak let diya changes korta para
    
    2 but const hoila value ba word ta same thakya jaibo changes koron jaito na possibel na eda
    
    3. no chnges n value for the const . let a kora jai

---
+++ js fodundation/hello.js/part1/changes.js
@@ -0,0 +1,12 @@
+let gamename = "call of duty";
+
+gamename = "pubg";
+
+console.log(gamename);
+
+const username = "debjit";
+
+// username = "das"; // This will throw an error because 'username' is a constant and cannot be reassigned.
+
+// username = "das";
+// console.log(username);
