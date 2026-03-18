b642c3cc62313bc958bcca33df1f5cec434802e2
debjitttdasss <rossdebb452@gmail.com>
2026-03-18 19:06:39 +0530

    for non-privitimes if js
    its diff from the primitives
    
    for the non primitives--
    1 username a jide asa orint iut hoibo
    clealy and than next
    2 array
    3 you can abkle to see the datatyppe by using the typeof cooamnd in which js is diff like the thre is isvalue  of 3daf in which this is what numder or sting
    
    js -- work like if numder in tyoeof is thre it ewill be numbder
    or is numder na thaka typeof a than eda hoito na numdert -- eda hoibo (string)

---
+++ js fodundation/hello.js/part2/nonprimitives.js
@@ -0,0 +1,32 @@
+let username = {
+    firstname: "debjit",
+     lastname: "das"
+};
+
+const myFistnmae = "debjit";
+username.firstname = "Ross";
+
+//console.log(username.["firstname"]);
+//console.log(username.["lastname"]);
+//console.log(username);
+//console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+
+// array 
+
+let anotherUser = ["debjit", "das", 28, true];
+
+
+// console.log(anotherUser[0]);
+
+let isValue = "3daf"; // eda true ra 1 loiya laise, false ra 0 loiya laise
+console.log(typeof Number(isValue)); // number
+console.log(Number(isValue)); // null
+console.log(Number(null)); // 0  
+
+
+// js ar kono  datatyoe nai eda hoisa dynamic programming language, kono variable ar type fixed na, value assign korar por type change hoite pare
+
