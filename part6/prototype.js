db31cc7f3e1c1a5384382afb527408ecc20f286d
debjitttdasss <rossdebb452@gmail.com>
2026-04-11 20:14:02 +0530

    part 6 oops concepts

---
+++ part6/prototype.js
@@ -0,0 +1,25 @@
+let computer = {
+  cpu: 12,
+};
+
+let lenovo = {
+  screem: "hd",
+
+  __proto__: computer,
+};
+let johnhardware = {};
+
+// console.log('lenovo', lenovo.__proto__);
+
+
+//console.log('tesla', Object.getPrototypeOf(tesla));
+
+let genericCar = { tyres: 4 };
+
+let tesla = {
+  driver: "Ai",
+};
+
+Object.setPrototypeOf(tesla, genericCar);
+
+console.log("tesla", Object.getPrototypeOf(tesla));
